a4a22a2aa486d39ca5e0d78a97963df04e1cd3ce
Gabriel <127705796+Beemuu@users.noreply.github.com>
2024-01-17 15:01:07 -0300

    Altura ta bugando/perde valores quando balanceia

--- ABB - Biblioteca.h
+++ ABB - Biblioteca.h
@@ -8,30 +8,13 @@ using namespace std;
 
 typedef struct TreeNo {
 	int info;
+	int altura;
 	struct TreeNo *Llink;
 	struct TreeNo *Rlink;
 } no;
 
 no *ini_AB(no *T) {
 	return NULL;
-}
-
-no *insere_AB(no *T, int x) {
-	if (T == NULL){
-		T = (no *) malloc (sizeof(no));
-		T->info = x;
-		T->Llink = NULL;
-		T->Rlink = NULL;
-		return T;
-	}
-	else {
-		if (x < T->info) 
-			T->Llink = insere_AB(T->Llink, x);
-		else {	 
-			T->Rlink = insere_AB(T->Rlink, x);
-		}
-		return T;
-	}
 }
 
 void emOrdem_AB(no *T) {
@@ -58,38 +41,79 @@ void posOrdem_AB(no *T) {
 	}
 }
 
-int altura_AB(no *T) {
-	int aL, aR;
-		
-	if(T == NULL) {
-		return -1;
-	}
-	else {
-		aL = altura_AB(T->Llink);
-		aR =  altura_AB(T->Rlink);
-		if (aL < aR){
-			return (aR+1);
-		}
-		else{
-			return (aL+1);
-		}
-	}
-}
-
-no *rodaDireita(no *p) {
-    no *temp, *q;
-	q = p->Llink;
-	temp = q->Rlink;
+no *rotacaoDireita(no *p) {
+	no *q = p->Llink;
+	no *temp = q->Rlink;
+	
 	q->Rlink = p;
 	p->Llink = temp;
+	
+	p->altura = altura(p);
+	q->altura = altura(q);
 	return q;
 }
 
-no *rodaEsquerda(no *p) {
-    no *temp, *q;
-	q = p->Rlink;
-	temp = q->Llink;
+no *rotacaoEsquerda(no *p) {
+	no *q = p->Rlink;
+	no *temp = q->Llink;
+	
 	q->Llink = p;
 	p->Rlink = temp;
+	
+	p->altura = altura(p);
+	q->altura = altura(q);
 	return q;
+}
+
+int maximo(int x, int y){
+	if (x > y)
+		return x;
+	else
+		return y;
+}
+
+int altura(no *p) {
+	if(p == NULL)
+		return -1;
+	else {
+		return (1+maximo(altura(p->Llink), altura(p->Rlink))) ;
+	}
+}
+
+int Fb(no *p){
+	return altura(p->Rlink) - altura(p->Llink);
+}
+
+no* balance(no *p){
+	if( Fb(p) == 2 ){
+		if( Fb(p->Rlink) < 0 )
+			p->Rlink = rotacaoDireita(p->Rlink);
+			return rotacaoEsquerda(p);
+	}
+		if( Fb(p) == -2 ){
+			if( Fb(p->Llink) > 0 )
+				p->Llink = rotacaoEsquerda(p->Llink);
+				return rotacaoDireita(p);
+		}
+	return p;
+}
+
+
+no *insere_AB(no *T, int x) {
+	if (T == NULL){
+		T = (no *) malloc (sizeof(no));
+		T->info = x;
+		T->Llink = NULL;
+		T->Rlink = NULL;
+		return T;
+	}
+	else {
+		if (x < T->info) 
+			T->Llink = insere_AB(T->Llink, x);
+		else {	 
+			T->Rlink = insere_AB(T->Rlink, x);
+		}
+	}
+	balance(T);
+	return T;
 }
